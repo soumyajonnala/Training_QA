70de8556ed16ce6a0e611d1b6da5a80315fadd13
Soumya <Soumya@DESKTOP-LQ844T8>
2017-07-14 11:46:47 -0400

    edited

--- Devansh/src/First/program/HelloWorld.java
+++ Devansh/src/First/program/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World");//welcome
 		System.out.println("hi");
-		/*welcome
+
 
 	}
 
