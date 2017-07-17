32f35c75199d4bd5a4ad42e9c6938c3d07fa8eac
Soumya <Soumya@DESKTOP-LQ844T8>
2017-07-17 13:08:44 -0400

    commited changes

---
+++ ApplicationTesting/src/test/java/TestingApps/UpdatedFibonacci.java
@@ -0,0 +1,23 @@
+package TestingApps;
+
+public class UpdatedFibonacci {
+
+	public static void main(String[] args) {
+		 int a,b,c;
+		 a=1;
+		 b=1;
+		 c=a+b;
+		 System.out.println(a);
+		 System.out.println(b);
+		 System.out.println("hi");
+		 while(c<=100){
+			 System.out.println(c);
+			 a=b;
+			 b=c;
+			 c=a+b;
+			 
+		 }
+
+	}
+
+}
